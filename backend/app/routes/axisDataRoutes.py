031f1629e0ca1ebad8b2ed75cee72e3c761e25b2
Sathishkumar-Ramachandran <sathish.spacy2001@gmail.com>
2024-03-28 15:00:08 +0530

    Latest One

--- backend/app/routes/axisDataRoutes.py
+++ backend/app/routes/axisDataRoutes.py
@@ -1,16 +1,37 @@
 from flask import Flask, request, Blueprint
 from datetime import date
 from app.services.getAxisDataservice import getAxisData
+from app.services.AxisServices.rawDataupload import upload_rawdata
+
+
 axisData = Blueprint('axisdata', __name__)
 
-@axisData.route('/rawdata/axis/<getdate>', methods=['GET'])
-def get_axisData(getdate):
-    if not getdate:
-        get_date = date.today()
-        try:
-            getAxisData(get_date)
-            return  {"status": "success", 200: 'Requested data for today.'},  200
-        except Exception as e:
-            return f'Error Occured during Axis Data Retrieval', 400
+@axisData.route('/rawdata/axis', methods=['GET'])
+def get_axisData():
+    try:
+        getdate = request.args.get('date') 
+        if not getdate:
+            getdate = str(date.today())
+        data = getAxisData(getdate)
+        return  data, 200
+        
+    except Exception as e:
+        print("Error in getting axis data",e)
+        return {
+                "status": "failure",
+                "message":"Failed to fetch Axis Data"
+               },500
 
-@axisData.route('/rawdata/axis', methods=['POST'])
+
+           
+
+@axisData.route('/rawdata/axis/upload', methods=['POST'])
+def upload_data():
+    payload_data = request.get_json()
+    if payload_data:
+        try:
+            upload_rawdata(payload_data)
+            return {'message': 'File uploaded successfully'}, 201
+        except Exception as e:
+            print("Unexpected error:", e)
+            return {'error':'There was an issue with the file you tried to upload.'},  500
---
+++ backend/app/services/AxisServices/rawDataupload.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config.mongoConnection import axis_raw_data_collection
+from datetime import date
+
+
+def upload_rawdata(payload):
+    uploadDate = str(date.today())
+    if payload:
+        try:
+            if axis_raw_data_collection.find_one({"date": uploadDate}):
+                axis_raw_data_collection.update_one({
+                    "date: ": uploadDate}, {"$push": {"rawdata": payload} 
+                })
+            else:
+                axis_raw_data_collection.insert_one({
+                    'date': uploadDate,
+                    'rawdata': payload
+                })
+                return {'message': f'Data for {uploadDate} has been uploaded Successfully'})
+        except Exception as e:
+            return f'Error occured during  data insertion - {e}'
